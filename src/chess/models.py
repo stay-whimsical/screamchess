0fb7561c517e9f75cd287e8de08711f0ac1c6573
Karen Castelletti <karen@paribus.co>
2017-08-20 16:24:06 -0400

    Added a Space class within Board, which encompasses the piece but also the present conditions of the space (ie whether under potential threat).

--- src/chess/models.py
+++ src/chess/models.py
@@ -58,24 +58,47 @@ class Move:
   def __str__(self):
     return str(self.from_loc) + "_to_" + str(self.to_loc)
 
+
+class Space:
+  def __init__(self, piece=None):
+     self.piece = piece 
+     self.danger_from_white = False
+     self.danger_from_black = False
+ 
+  def __str__(self):
+    output = str(self.piece) if self.piece else '_'
+    output += 'whitethreat' if str(self.danger_from_white) else ''
+    output += 'blackthreat' if str(self.danger_from_black) else ''
+    return output 
+
 class Board:
   def __init__(self):
-    self.state = [[Rook('white'), Knight('white'), Bishop('white'), Queen('white'), King('white'), Bishop('white'), Knight('white'), Rook('white')],
-                  [Pawn('white'), Pawn('white'), Pawn('white'), Pawn('white'), Pawn('white'), Pawn('white'), Pawn('white'), Pawn('white')],
-                  [None]*8,
-                  [None]*8,
-                  [None]*8,
-                  [None]*8,
-                  [Pawn('black'), Pawn('black'), Pawn('black'), Pawn('black'), Pawn('black'), Pawn('black'), Pawn('black'), Pawn('black')],
-                  [Rook('black'), Knight('black'), Bishop('black'), Queen('black'), King('black'), Bishop('black'), Knight('black'), Rook('black')]]
+    # The indices are:  [[(0,0), (0,1), ... (0,7)],
+    #                    [(1,0) (1,1) ... (1,7)],
+    #                           ....
+    #                    [(7,0), (7,1) ... (7,7)]]
+    self.state = [[]]
+    for row in [[Rook('white'), Knight('white'), Bishop('white'), Queen('white'), King('white'), Bishop('white'), Knight('white'), Rook('white')],
+                [Pawn('white'), Pawn('white'), Pawn('white'), Pawn('white'), Pawn('white'), Pawn('white'), Pawn('white'), Pawn('white')],
+                [None]*8,
+                [None]*8,
+                [None]*8,
+                [None]*8,
+                [Pawn('black'), Pawn('black'), Pawn('black'), Pawn('black'), Pawn('black'), Pawn('black'), Pawn('black'), Pawn('black')],
+                [Rook('black'), Knight('black'), Bishop('black'), Queen('black'), King('black'), Bishop('black'), Knight('black'), Rook('black')]]:
+      for piece in row:
+        if len(self.state[-1]) == 8:
+          self.state.append([])
+        self.state[-1].append(Space(piece))
+    
     self.moves = []
     self.turn = 'white'
 
   def __str__(self):
     output = ''
     for row in self.state:
-      for piece in row:
-        output += (str(piece) if piece else '_') + ' '
+      for space in row:
+        output += (str(space.piece) if space.piece else '_') + ' '
       output += '\n'
     return output
 
@@ -84,14 +107,14 @@ class Board:
       exec_move(move)
 
   def exec_move(self, move):
-    moving_piece = self.state[move.from_loc[0]][move.from_loc[1]]
-    displaced_piece = self.state[move.to_loc[0]][move.to_loc[1]]
+    moving_piece = self.state[move.from_loc[0]][move.from_loc[1]].piece
+    displaced_piece = self.state[move.to_loc[0]][move.to_loc[1]].piece
     assert moving_piece
     assert moving_piece.color == self.turn
-    self.state[move.to_loc[0]][move.to_loc[1]] = self.state[move.from_loc[0]][move.from_loc[1]]
-    self.state[move.from_loc[0]][move.from_loc[1]] = None
-    if old_piece:
-      print "MURDERED " + str(old_piece)
+    self.state[move.to_loc[0]][move.to_loc[1]].piece = moving_piece
+    self.state[move.from_loc[0]][move.from_loc[1]].piece = None
+    if displaced_piece:
+      print "MURDERED " + str(displaced_piece)
      
      
 
