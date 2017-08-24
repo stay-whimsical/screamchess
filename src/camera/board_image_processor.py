32358c7ab2b55f146f6b5f70070f9c95ad241193
Sara Drakeley <sdrakeley@eglp.com>
2017-08-24 04:07:08 -0400

    Board Image Processor: The consolidation of the tinkering

---
+++ src/camera/board_image_processor.py
@@ -0,0 +1,201 @@
+"""
+This module contains the main image processing class for the current
+setup with the camera beneath the chess board. It uses colors
+to distinguish among the pieces. It also has a setup mode to
+initialize the location of the center of each square on the board.
+"""
+import cv2
+import numpy as np
+import bisect
+import operator
+from src.camera.setup_board import get_square_centers_from_board
+
+# TODO: refactor to put all code and magic numbers in here, but for now
+#       just importing the other working modules
+
+
+class BoardProcessor:
+    """
+    You can poll the BoardProcessor at any time to get the current
+    state from the last image it successfully processed.
+    """
+
+    def __init__(self, debug_image_mode=False):
+        self._num_squares = 8  # Width of board, standard chess board
+        self._contour_len_threshold = 10  # Perimeter of shape in pixels
+        self._centers = []
+        self._cur_state = None
+        self._debug_images = debug_image_mode
+        self._color_map = {'WB': (np.array([110, 50, 50]),  # light blue
+                                  np.array([130, 255, 255])),
+                           # Also BK with dot
+                           'BB': (np.array([222, 50, 50]),  # dark blue
+                                  np.array([243, 255, 255])),
+                           # Also BR with dot
+                           'WP': (np.array([57, 50, 50]),  # green
+                                  np.array([96, 255, 255])),
+                           # also BQ with dot
+                           'WR': (np.array([110, 50, 50]),  # purple
+                                  np.array([130, 255, 255])),
+                           # also WK with dot - this seems to be off
+                           'BP': (np.array([351, 50, 50]),  # red
+                                  np.array([360, 255, 255])),
+                           # also WQ with dot
+                           'WP': (np.array([25, 50, 50]),  # yellow
+                                  np.array([55, 255, 255])),
+                           # also WK with dot
+                           }
+
+    @staticmethod
+    def _open_image(image_path):
+        return cv2.imread(image_path)
+
+    def _show_image(self, im_array, title='image'):
+        if self._debug_images:
+            cv2.imshow(title, im_array)
+            cv2.waitKey(0)
+
+    def setup_board(self, image_path):
+        """Get the center of each square from a given board image,
+        corresponding to it's i-j position in a 2d array (representing
+        the board state).
+        """
+        self._centers = self._sort_centers(
+            get_square_centers_from_board(image_path, self._num_squares))
+
+    def _sort_centers(self, centers):
+        """Sort the centers into a 2D array.
+
+        Take the (min, min), and then for each point get the vector from the
+        min to that point. Sort the vectors by length, and multiply by the
+        jacobian. Just kidding. I'm not clever.
+
+        If everything were not warped, we would be making a transformation
+        from (i*w, j*h) = i, j     -> However, things are warped. So. Nope.
+
+        Alright, what if we just sort by y, break it into rows of 8, then
+        sort by x, and then store point -> (i, j) pair
+
+        Return:
+            The 2D array of the center points, in their proper i,j position.
+        """
+        center_dict = {}
+        center_array = []  # purely for quick visualization and debugging
+        sorted_by_y = centers[:]
+        sorted_by_y.sort(key=operator.itemgetter(1))
+        for i in range(self._num_squares):
+            row = sorted_by_y[i*self._num_squares:(i+1)*self._num_squares]
+            row.sort()
+            for j, point in enumerate(row):
+                center_dict[point] = (i, j)
+            center_array.append(row)
+        if self._debug_images:
+            print 'Got centers of board:'
+            for row in center_array:
+                print row
+        return center_dict
+
+    def _point_within_square(self, shape_center, half_square_threshold):
+        """Get the i,j coordinate of the square center closest to this point
+        and test whether it is within the half-square threshold"""
+        dists = []
+        for square_center in self._centers:
+            bisect.insort(dists, (np.linalg.norm(np.array(square_center) -
+                                                 np.array(shape_center)),
+                                  square_center))
+        assert len(dists) > 0, ('Got no dists from ', str(shape_center),
+                                str(self._centers))
+        if dists[0][0] < half_square_threshold:
+            print('dists [0] = ', dists[0], '< ', half_square_threshold)
+            return self._centers[dists[0][1]]
+
+    def get_board_state(self, image_path):
+        """Return the state of the board (in terms of pieces in each
+        square) from the given board image.
+
+        Centers are np.arrays, which have point-like behavior for subtraction.
+        """
+        image = self._open_image(image_path)
+        height, width, channels = image.shape
+        half_square_threshold = height / (self._num_squares * 2)
+        # Fun python fact - the code below makes copies of each list
+        # board = [ [0]*self._num_squares ]*self._num_squares
+        board = [ [0 for x in xrange(self._num_squares)]
+                  for x in xrange(self._num_squares)]
+        # Get a convolved image for each color in our color map
+        for piece, color_range in self._color_map.items():
+            filtered_image = self._get_convolved_image(image, color_range)
+            if self._debug_images:
+                print('Now for piece', piece, 'color range', color_range)
+                self._show_image(filtered_image, 'convolved')
+            # I love O(n**2). As sweet as microwaved bananas.
+            for shape_center in self._get_centers(filtered_image):
+                print('Now for shape center', shape_center)
+                coords = self._point_within_square(shape_center,
+                                                   half_square_threshold)
+                print('coordinate = ', coords)
+                if coords is not None:
+                    board[coords[0]][coords[1]] = piece
+        return board
+
+    def _get_convolved_image(self, image, color_range):
+        """Get an image mask filtered to just the locations of a certain
+        color within the provided color range.
+        """
+        lower, upper = color_range
+        # put into HSV color space because hue is more reliable - RGB varies
+        # based on brightness - so we would have lots of possible values
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        self._show_image(hsv, 'HSV')
+
+        # Threshold the HSV image to get only colors within the range
+        mask = cv2.inRange(hsv, lower, upper)  # TODO is it actually more
+                                               #      optimal to overwrite?
+        self._show_image(mask, 'Masked by ' + str(lower) + str(upper))
+
+        # Blur the mask - this helps bring out more of the colored pixels
+        blurred = cv2.GaussianBlur(mask, (5, 5), 0)
+        self._show_image(blurred, 'Blurred')
+
+        # Dilate the image to make the blue spots bigger
+        # TODO: figure out which dilate configuration makes the best shapes
+        kernel = cv2.getStructuringElement((cv2.MORPH_DILATE), (4, 4))
+        dilated = cv2.dilate(blurred, kernel, iterations=5)
+        self._show_image(dilated, 'Dilated')
+
+        # TODO: threshold one more time
+        thresh = 127
+        im_bw = cv2.threshold(dilated, thresh, 255, cv2.THRESH_BINARY)[1]
+        self._show_image(im_bw, 'Threshold')
+
+        return im_bw
+
+    def _get_centers(self, image):
+        """Image needs to be a masked image, easily contourizable."""
+        centers = []
+        cnt_im_array, contours, heirarchy = cv2.findContours(
+            image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Filter out small contours around noise
+        cnts = [c for c in contours if len(c) > self._contour_len_threshold]
+        for c in cnts:
+            # compute the center of the contour
+            M = cv2.moments(c)
+            denom = M["m00"]
+            if denom == 0:
+                continue
+            cX = int(M["m10"] / denom)
+            cY = int(M["m01"] / denom)
+
+            # Insert center into our list of centers
+            bisect.insort(centers, (cX, cY))
+
+            # draw the contour and center of the shape on the image
+            cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+            cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
+            cv2.putText(image, "center", (cX - 20, cY - 20),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+        self._show_image(image, 'Centers')
+        return centers
+
--- src/camera/setup_board.py
+++ src/camera/setup_board.py
@@ -1,39 +1,12 @@
 """
-TODO: Instead, use image convolutions to really isolate the shapes
-of the blue squares, and then use the contours and get the centers of
-the shapes.
-
-
-    dilate(src, kernel[, dst[, anchor[, iterations[, borderType[, borderValue]]]]]) -> dst
-    .   @brief Dilates an image by using a specific structuring element.
-    .
-    .   The function dilates the source image using the specified structuring element that determines the
-    .   shape of a pixel neighborhood over which the maximum is taken:
-    .   \f[\texttt{dst} (x,y) =  \max _{(x',y'):  \, \texttt{element} (x',y') \ne0 } \texttt{src} (x+x',y+y')\f]
-    .
-    .   The function supports the in-place mode. Dilation can be applied several ( iterations ) times. In
-    .   case of multi-channel images, each channel is processed independently.
-    .
-    .   @param src input image; the number of channels can be arbitrary, but the depth should be one of
-    .   CV_8U, CV_16U, CV_16S, CV_32F or CV_64F.
-    .   @param dst output image of the same size and type as src\`.
-
-    .   @param kernel structuring element used for dilation; if elemenat=Mat(), a 3 x 3 rectangular
-    .   structuring element is used. Kernel can be created using getStructuringElement
-    .   @param anchor position of the anchor within the element; default value (-1, -1) means that the
-    .   anchor is at the element center.
-    .   @param iterations number of times dilation is applied.
-    .   @param borderType pixel extrapolation method, see cv::BorderTypes
-    .   @param borderValue border value in case of a constant border
-    .   @sa  erode, morphologyEx, getStructuringElement
-(END)
+This module uses OpenCV to process a setup state image in order to find the
+centers of each square. The expectation is that blue sticky notes will be
+cut up into squares and placed at the center of each square.
 
 """
 import cv2
 import numpy as np
 import bisect
-from collections import Counter, defaultdict
-
 
 def open_image(image_path):  # TODO delete
     return cv2.imread(image_path)
@@ -41,7 +14,7 @@ def open_image(image_path):  # TODO delete
 def get_square_centers_from_board(image_path, num_squares, show_images=False):
     """
 
-    TODO: less copy pasta from process_board
+    TODO: move this all over into board_image_processor
 
     :param state:
     :param i:
@@ -51,7 +24,6 @@ def get_square_centers_from_board(image_path, num_squares, show_images=False):
     """
     image = open_image(image_path)
     height, width, channels = image.shape
-    print('img height and widht = ', height, width)
 
     contour_len_threshold = 5
 
@@ -67,28 +39,21 @@ def get_square_centers_from_board(image_path, num_squares, show_images=False):
     # put into HSV color space because hue is more reliable - RGB varies
     # based on brightness - so we would have lots of possible values
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    print('Got openCV image')
     if show_images:
         show_image(hsv)
 
-    # Blur the image to reduce some of the noise
-    # Blurring before the mask actually super increased the noise
-    # blurred = cv2.GaussianBlur(hsv, (5, 5), 0)
-    # if show_images:
-    #     show_image(blurred)
-
     # Threshold the HSV image to get only colors within the range
     mask = cv2.inRange(hsv, lower, upper)
-    print('Got mask')
     if show_images:
         show_image(mask)
 
+    # Blur the mask - this helps bring out some of the darker stickies
     mask = cv2.GaussianBlur(mask, (5, 5), 0)
     if show_images:
         show_image(mask)
 
-
     # Dilate the image to make the blue spots bigger
+    # TODO: figure out which dilate configuration makes the best shape
     kernel = cv2.getStructuringElement((cv2.MORPH_DILATE), (4, 4))
     mask = cv2.dilate(mask, kernel, iterations=5)
     if show_images:
@@ -125,128 +90,9 @@ def get_square_centers_from_board(image_path, num_squares, show_images=False):
     if show_images:
         show_image(mask)
 
-
-
-    # print('got centers = ', centers)
-    # center_dict = dedupe_centers(centers, height, num_squares)
-    # print('got center dict = ', center_dict)
-    # center_dict = insert_centers(center_dict, height, num_squares)
-    # print('now center dict = ', center_dict)
-
-
-    # for x, col in center_dict.items():
-    #     for y in col:
-    #         cv2.circle(mask, (x, y), 7, (255, 255, 255), -1)
-    #         cv2.putText(mask, "center", (x - 20, y - 20),
-    #                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-    #     print('got center at ', cX, cY)
-    #     if show_images:
-    #         show_image(mask)
-
-    print('Centers =', centers)
     return centers
 
 
-# def dedupe_centers(centers, height, num_squares):
-#     distance_threshold = 10.0  # euclidian pixel distance to dedupe
-#
-#     to_dedupe = defaultdict(list)
-#     # Note - this is quadratic right now - there's certainly a way to
-#     # optimize this, but I am just brute forcing it for POC
-#     #
-#     for center in centers:
-#         for neighbor in centers:
-#             if ((neighbor[0] - center[0]) ** 2 + (
-#                 neighbor[1] - center[1]) ** 2) ** 0.5 < distance_threshold:
-#                 to_dedupe[center].append(neighbor)
-#     for dupe in to_dedupe:
-#
-
-
-
-
-def dedupe_centers_retired(centers, height, num_squares):
-    """
-    This is actually the wrong way to do it - it tries to combine everything
-    into an axis aligned grid - but that's exactly why we're NOT doing it this
-    way - we want to have exact placements for the centers.
-
-    :param centers:
-    :return: Dictionary of x vals to column of y values
-    """
-    square_size = float(height/num_squares)
-    x_threshold = 5  # pixels to filter for noise
-    y_threshold = 5
-    c_dict = defaultdict(list)
-    for x, y in centers:
-        c_dict[x].append(y)
-    sorted_keys = c_dict.keys()[:]
-    sorted_keys.sort()
-    # combine by x
-    deduped_x = defaultdict(list)
-    prev_val = sorted_keys[0]
-    print 'now combining by x', sorted_keys
-    for i, val in enumerate(sorted_keys[1:]):
-        print '\t for i, val', i, val
-        if val - prev_val > square_size - x_threshold:
-            print '\t\t \033[36;1m far enough away. adding', prev_val, '\033[0m'
-            deduped_x[prev_val].extend(c_dict[prev_val])
-            prev_val = val
-        else:
-            print '\t\t \033[31;1m NOT far enough away. combining val', val, 'with prev_val', prev_val, '\033[0m'
-            prev_val_col = c_dict[prev_val]  # store column to combine
-            prev_val = prev_val + (val - prev_val) / 2
-            # Store the combined lists of the previous y columns
-            c_dict[prev_val] = c_dict[val] + prev_val_col
-    # TODO figure out how to dedupe the last one...
-    print('adding last prev_val', prev_val)
-    deduped_x[prev_val] = c_dict[prev_val]
-    res_x = deduped_x.keys()
-    res_x.sort()
-    print('res_x = ', res_x)
-
-    res = {}
-    # Do the same for each y column
-    for x, col in deduped_x.items():
-        sorted_col = col[:]
-        sorted_col.sort()
-        deduped_y = []
-        prev_val = sorted_col[0]
-        for i, val in enumerate(sorted_col[1:]):
-            if val - prev_val > square_size - y_threshold:
-                deduped_y.append(prev_val)
-                prev_val = val
-            else:
-                prev_val = prev_val + (val - prev_val) / 2
-        # TODO figure out how to dedupe the last one
-        deduped_y.append(prev_val)
-        res[x] = deduped_y
-    return res
-
-def insert_centers(found_centers, height, num_squares):
-    # TODO use height in check?
-    assert len(found_centers) == num_squares, 'Missing whole columns'
-    threshold = 10
-    full_rows = {x: col for x, col in found_centers.items()
-                 if len(col) == num_squares}
-    for x, col in found_centers.items():
-        if len(col) == num_squares:
-            continue
-        if len(col) < num_squares:
-            for i, val in enumerate(col):
-                if len(col) == num_squares:  # TODO: better loop construction
-                    break
-                row_vals = [full[i] for full in full_rows.values()]
-                avg_value = sum(row_vals) / float(len(col))
-                if abs(avg_value - val) < threshold:
-                    continue
-                else:
-                    bisect.insort(col, int(avg_value))
-        assert len(col) == num_squares, 'incorrect number values for column'
-        full_rows[x] = col
-    return full_rows
-
-
-def show_image(im_array):
-    cv2.imshow('image', im_array)
+def show_image(im_array, title='image'):
+    cv2.imshow(title, im_array)
     cv2.waitKey(0)
